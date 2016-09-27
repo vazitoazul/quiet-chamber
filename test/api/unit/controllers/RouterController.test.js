8e732926b1c986cdd7a7536deaf8a3830e2bb3f2
donalbertico <atates@udlanet.ec>
2016-09-26 23:18:42 -0500

    adding unit test for redirect method on routerController.js

--- test/api/unit/controllers/RouterController.test.js
+++ test/api/unit/controllers/RouterController.test.js
@@ -7,6 +7,21 @@ describe('RouterController', function() {
         .get('/')
         .expect(200, done);
     });
-  });
 
-})
+    it('(5 > url lentgh) should render homepage with the not found fragment', function (done) {
+	      request(sails.hooks.http.app)
+	        .get('/user?id=xxxx')
+	        .expect(function(res){
+	        	if(!res.text.includes("<title>My App</title>")) throw new Error('oh shit');
+	        })
+	        .end(done)
+    });
+
+    it('should return 404 not found status', function (done) {
+	      request(sails.hooks.http.app)
+	        .get('/nourl')
+	        .expect(404,done);
+    });
+  });
+});
+
