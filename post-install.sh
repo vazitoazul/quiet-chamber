4a62a45f5ad85d7d2f798409b60860b5e5e29c74
Juan Rosero <jfrosero@udlanet.ec>
2017-07-13 16:29:14 -0500

    Moved alfacoin file where it is visible and copied when build finishes

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,5 @@
 ./node_modules/.bin/bower install
 ./node_modules/.bin/polymer build
 cp build/bundled/index.html views/homepage.ejs
+cp ALFAcoins_5967e16c1c01e5967e16c1c0545967e16c1c089.txt build/bundled/ALFAcoins_5967e16c1c01e5967e16c1c0545967e16c1c089.txt
 echo "BUILD SUCESS"
